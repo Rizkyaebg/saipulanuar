412cfe36ce4da073825700a0fa108783b220a8da
Rizkyaebg <97835768+Rizkyaebg@users.noreply.github.com>
2022-01-16 21:11:35 +0700

    Create Ind.js

---
+++ kode ulang/Ind.js
@@ -0,0 +1,148 @@
+exports.donasi = () => { 
+return`*HAI KAK MAU BENERAN DONASI ATAU TIDAK KAK*
+*SEMOGA TIDAK DILIHAT DOANG*
+
+*PAYMENT BOT*
+DANA: (0882-7926-8363)
+OVO: VIA QR
+PULSA: VIA QR AJA
+
+*TERIMA KASIH KAK*
+*LOVE YOU😋*`
+}
+exports.menu = (prefix, simbol) => { 
+return`=====[ 𝗟𝗶𝘀𝘁 𝗠𝗲𝗻𝘂 ]=====
+
+❏ 𝙊𝙩𝙝𝙚𝙧𝙨
+${simbol} ${prefix}info
+${simbol} ${prefix}cekprem
+${simbol} ${prefix}scbot
+${simbol} ${prefix}owner
+${simbol} ${prefix}donasi
+${simbol} ${prefix}request <fitur>
+
+❏ 𝙊𝙬𝙣𝙚𝙧.
+${ simbol }  ${ awalan } diri
+${ simbol }  ${ awalan } publik
+${ simbol }  ${ awalan } readall
+${ simbol }  ${ awalan } bc <teks>
+${ simbol }  ${ awalan } setfake <gambar balasan>
+${ simbol }  ${ awalan } setreply <nama>
+${ simbol }  ${ awalan } setthumb <gambar balasan>
+${ simbol }  ${ awalan } larangan 628xxx
+${ simbol }  ${ awalan } buka blokir 628xxx
+${ simbol }  ${ awalan } addprem 628xxx
+${ simbol }  ${ awalan } delprem 628xxx
+❏ 𝙂𝙧𝙤𝙪𝙥.
+${ simbol }  ${ awalan } tarif
+${ simbol }  ${ awalan } apakah
+${ simbol }  ${ prefix } berlangsung
+${ simbol }  ${ awalan } kapankah
+${ simbol }  ${ awalan } antilink
+${ simbol }  ${ awalan } cuti
+${ simbol }  ${ awalan } closegc
+${ simbol }  ${ awalan } opengc
+${ simbol }  ${ awalan } linkgrup
+${ simbol }  ${ awalan } listadmin
+${ simbol }  ${ awalan } setpp
+${ simbol }  ${ awalan } kick @tag
+${ simbol }  ${ awalan } tambahkan +628
+${ simbol }  ${ awalan } tagall
+${ simbol }  ${ awalan } hidetag
+${ simbol }  ${ awalan } demote
+${ simbol }  ${ awalan } promosi
+${ simbol }  ${ awalan } hapus
+❏ 𝘾𝙤𝙣𝙫𝙚𝙧𝙩.
+${ simbol }  ${ awalan } ttp
+${ simbol }  ${ awalan } attp
+${ simbol }  ${ awalan } tantangan
+${ simbol }  ${ awalan } kebenaran
+${ simbol }  ${ awalan } stiker <balas>
+${ simbol }  ${ awalan } toimg <balas>
+${ simbol }  ${ awalan } puisi
+${ simbol }  ${ awalan } lidah
+${ simbol }  ${ awalan } pantun
+${ simbol }  ${ awalan } nuliskiri
+${ simbol }  ${ awalan } nuliskanan
+${ simbol }  ${ awalan } foliokiri
+${ simbol }  ${ awalan } foliokanan
+❏ 𝘿𝙤𝙬𝙣𝙡𝙤𝙖𝙙.
+${ simbol }  ${ awalan } igdl <link>
+${ simbol }  ${ awalan } ttdl <link>
+${ simbol }  ${ awalan } tiktok <link>
+${ simbol }  ${ awalan } ytmp3 <link>
+${ simbol }  ${ awalan } ytmp4 <link>
+${ simbol }  ${ awalan } ytplay <judul>
+${ simbol }  ${ awalan } playmp4 <judul>
+${ simbol }  ${ awalan } playmp3 <judul>
+${ simbol }  ${ awalan } mediafire <link>
+${ simbol }  ${ awalan } instagram <link>
+${ simbol }  ${ awalan } tiktoknowm <link>
+
+❏ 𝙎𝙚𝙖𝙧𝙘𝙝.
+${simbol} ${awalan}waifu
+${simbol} ${awalan}husbu
+${simbol} ${awalan}pahlawan
+${simbol} ${prefix}detil pahlawan
+${simbol} ${prefix}ytsearch
+${simbol} ${awalan}pinterest
+${simbol} ${prefix}playstore
+${simbol} ${awalan}nekonime
+${simbol} ${awalan}namaninja
+${simbol} ${prefix}imgsearch
+${simbol} ${prefix}searchgrup
+
+❏ 𝘾𝙖𝙣𝙫𝙖𝙨.
+${simbol} ${awalan}rem
+${simbol} ${awalan}gura
+${simbol} ${prefix}sert1
+${simbol} ${prefix}sert2
+${simbol} ${prefix}sert3
+${simbol} ${awalan}kaneki
+${simbol} ${awalan}anak sadis
+${simbol} ${awalan}girlneko
+${simbol} ${awalan}pembuat loli
+
+❏ 𝙍𝙖𝙣𝙙𝙤𝙢.
+${simbol} ${awalan}rubah
+${simbol} ${awalan}kucing
+${simbol} ${awalan}anjing
+${simbol} ${awalan}birb
+${simbol} ${awalan}koala
+${simbol} ${awalan}panda
+${simbol} ${awalan}meme
+${simbol} ${awalan}nickepep
+${simbol} ${awalan}katalham
+${simbol} ${awalan}panda merah
+${simbol} ${prefix}estetikpic
+${simbol} ${awalan}lelucon gelap
+${simbol} ${awalan}memeindo
+${simbol} ${awalan}katasindiran
+
+❏ 𝙉𝙨𝙛𝙬.
+${simbol} ${prefix}perangkap
+${simbol} ${awalan}neko
+${simbol} ${awalan}awoo
+${simbol} ${awalan}waifu
+${simbol} ${awalan}blowjob
+${simbol} ${awalan}megumin
+${simbol} ${awalan}shinobu
+
+
+❏ 𝙈𝙖𝙠𝙚𝙧.
+${simbol} ${awalan}bneon
+${simbol} ${awalan}matriks
+${simbol} ${prefix}breakwall
+${simbol} ${prefix}tetesan air
+${simbol} ${prefix}lithgtext
+${simbol} ${prefix}krisma
+${simbol} ${awalan}tfire
+${simbol} ${awalan}sandw
+${simbol} ${prefix}text3dbox
+${simbol} ${awalan}tersisa
+${simbol} ${awalan}cahaya
+${simbol} ${awalan}nulis
+${simbol} ${awalan}blackpink
+${simbol} ${awalan}lampu_neon
+${simbol} ${awalan}permainan
+${simbol} ${awalan}cat air`
